2a1dd9992b9973a564dd2cbbc2fd9ca3ab1c4ff5
ntmonov <ntm@mail.bg>
2019-11-22 08:57:58 +0200

    added header/footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { LandingModule } from './components/landing/landing.module'
+import { SharedModule } from './components/shared/shared.module'
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { LandingModule } from './components/landing/landing.module'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LandingModule
+    LandingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
