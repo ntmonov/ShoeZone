68e47f0839606108f52566e5cc9c6428c3eedfa2
ntmonov <ntm@mail.bg>
2019-12-10 14:09:11 +0200

    added cart view template

--- src/app/components/cart/cart/cart.component.ts
+++ src/app/components/cart/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/core/services/cart.service';
+import { Shoe } from 'src/app/core/models/shoe';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-cart',
@@ -8,13 +10,23 @@ import { CartService } from 'src/app/core/services/cart.service';
 })
 export class CartComponent implements OnInit {
 
-  products: Object
+  productIds: string[]
+  products: Shoe[] = []
+  cartIds$
   cart$
 
-  constructor(private cartService: CartService) { }
+  constructor (private cartService: CartService) { }
 
-  ngOnInit() {
-    this.cart$ = this.cartService.getCartProducts().subscribe(data => this.products = data, err => console.log(err))
+  async ngOnInit () {
+    this.productIds = await this.cartService.getCartProducts().toPromise()
+    const ids = []
+    for (let cartItem of this.productIds) {
+      ids.push(cartItem['productId'])
+    }
+    for (let id of ids) {
+      const prod = await this.cartService.getProductById(id).toPromise()
+      this.products.push(prod)
+      console.log(prod)
+    }
   }
-
 }
--- src/app/core/services/cart.service.ts
+++ src/app/core/services/cart.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core'
 import { HttpHeaders, HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
+import { Shoe } from '../models/shoe'
 
 @Injectable({
   providedIn: 'root'
@@ -33,6 +35,16 @@ export class CartService {
       })
     }
     const userId = window.sessionStorage.getItem('id')
-    return this.http.get(`${this.URL}appdata/${this.APP_KEY}/cart?query={"userId":"${userId}"}&fields=productId`, httpHeaders)
+    return this.http.get<string[]>(`${this.URL}appdata/${this.APP_KEY}/cart?query={"userId":"${userId}"}`, httpHeaders)
+  }
+
+  getProductById (shoeId: string) {
+    const httpHeaders = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Kinvey ' + window.sessionStorage.getItem('token')
+      })
+    }
+    return this.http.get<Shoe>(`${this.URL}appdata/${this.APP_KEY}/shoes?query={"_id":"${shoeId}"}`, httpHeaders)
   }
 }
